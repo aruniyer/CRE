cdd5df073b0eca45b4d828c9596c3e41cfd5f282
aruniyer <arunbcn@familyedge-dl.barcelona.corp.yahoo.com>
2014-12-23 17:05:56 +0100

    removed the sdp solver from consistency

--- src/iitb/olap/MMDConsistency.java
+++ src/iitb/olap/MMDConsistency.java
@@ -1,10 +1,7 @@
 package iitb.olap;
 
 import java.util.Arrays;
-import java.util.LinkedHashMap;
 
-import Jama.Matrix;
-import gnu.trove.list.array.TIntArrayList;
 import iitb.olap.Config.DataHandler;
 import iitb.shared.optimization.GradientCompute;
 import iitb.shared.optimization.MirrorDescentTrainer;
@@ -34,184 +31,12 @@ public class MMDConsistency {
         this.dataHandler = dataHandler;
 
         this.scheme = scheme;
-//        if (this.scheme.equals(WeightScheme.DISPARITYSCHEME)) {
-//            doWeightLearning();
-//        }
         double[] x = new double[b.length * A.length];
         Arrays.fill(x, 1.0 / A.length);
         this.objective = doBlockCoordinateGradientDescent(x);
         this.estimates = x;
     }
     
-    private LinkedHashMap<TIntArrayList, Integer> groupIndex;
-    private double[] weights;
-    private void doWeightLearning() {
-	System.out.println("Learning weights for disparity ... ");
-	System.out.println("Building index for group ... ");
-        groupIndex = new LinkedHashMap<TIntArrayList, Integer>();
-        int indexer = 0;
-        for (int level = 1; level <= dataHandler.getNumAttributesGrouped() + 1; level++) {
-            for (int[] baseIndicator : dataHandler.getIteratorForLevel(level)) {
-                groupIndex.put(new TIntArrayList(baseIndicator), indexer);
-                indexer++;
-            }
-        }
-        System.out.println("Total number of groups : " + indexer);
-        weights = new double[indexer];
-        Arrays.fill(weights, 1);
-        double[] x = new double[weights.length];
-        Arrays.fill(x, 1);
-        GradientCompute gradientCompute = new DisparityGradientComputer(100);
-        System.out.println("Doing projected subgradient descent ... ");
-        double[] minEstimate = x.clone();
-        try {
-            System.out.println("start");
-            // subgradient descent
-            double alpha_k = 0.1;
-            double[] gk = new double[x.length];
-            gradientCompute.computeFunctionGradient(x, gk);
-            double R = 0;
-            for (int i = 0; i < gk.length; i++) {
-                R += gk[i] * gk[i];
-            }
-            R = alpha_k * 1000 * Math.sqrt(R);
-            double normgk = 0;
-            double fkbest = Double.MAX_VALUE / 4;
-            double lkbest = -Double.MAX_VALUE / 4;
-            double lk = 0;
-            double sumAlphak = 0;
-            
-            double prevGap = Double.MAX_VALUE;
-            int j;
-            double fk = -1;
-            for (j = 1; j <= 1000 && prevGap - fkbest + lkbest > 1e-2; j++) {
-                System.out.println("1+");
-                prevGap = fkbest - lkbest;
-                Arrays.fill(gk, 0);
-                fk = gradientCompute.computeFunctionGradient(x, gk);
-                System.out.println("1+");
-                if (fk < fkbest) {
-                    fkbest = fk;
-                    minEstimate = x.clone();
-                }
-                
-                normgk = 0;
-                for (int k = 0; k < gk.length; k++) {
-                    normgk += gk[k] * gk[k];
-                }
-                normgk = Math.sqrt(normgk);
-                System.out.println("1+");
-                
-                lk = (lk * 2 * sumAlphak + 2 * alpha_k * fk - alpha_k * alpha_k * normgk * normgk) / (2 * (sumAlphak + alpha_k));
-                if (j == 1)
-                    lk -= R * R / (2 * alpha_k);
-                sumAlphak += alpha_k;
-                lkbest = Math.max(lkbest, lk);
-                
-                for (int i = 0; i < x.length; i++) {
-                    x[i] = Math.max(0, x[i] - alpha_k * gk[i]);
-                }
-                System.out.println("1+");
-
-//                if (j%100 == 0 || j == 1)
-                    System.out.println("iter =  " + j + ", fk = " + fk + " :: gradnorm = " + normgk + " :: fkbest = " + fkbest + " :: lkbest = " + lkbest);
-            }
-            System.out.println("iter =  " + j + ", fk = " + fk + " :: gradnorm = " + normgk + " :: fkbest = " + fkbest + " :: lkbest = " + lkbest);
-        } catch (Exception exception) {
-            throw new RuntimeException("Error while learning weights!", exception);
-        }        
-        for (int i = 0; i < weights.length; i++) {
-            this.weights[i] = minEstimate[i];
-        }
-        System.out.println(Arrays.toString(weights));
-        System.out.println("optimization complete.");
-    }
-    
-    private class DisparityGradientComputer implements GradientCompute {
-
-	private float B;
-	
-	DisparityGradientComputer(float B) {
-	    this.B = B;
-	}
-	
-        @Override
-        public double computeFunctionGradient(double[] lambda, double[] grad) throws Exception {
-            double obj = computeFirstGradient(lambda, grad);
-            obj += computeSecondGradient(lambda, grad);
-            return obj;
-        }
-        
-        private double computeFirstGradient(double[] weights, double[] coeffs) {
-            double sum = 0;
-            for (int i = 0; i < weights.length; i++) {
-                coeffs[i] = Math.signum(weights[i]);
-                sum += Math.abs(weights[i]);
-            }
-            return sum;
-        }
-
-        private double computeSecondGradient(double[] weights, double[] coeffs) {
-            double[][] C = new double[nu.length][nu.length];
-            for (int j = 0; j < C.length; j++) {
-                for (int k = 0; k < C.length; k++) {
-                    for (int level = 1; level <= dataHandler.getNumAttributesGrouped() + 1; level++) {
-                        for (int[] baseIndicator : dataHandler.getIteratorForLevel(level)) {
-                            if (baseIndicator[j] == 1 && baseIndicator[k] == 1) {
-                                double sum = 0;
-                                for (int i = 0; i < baseIndicator.length; i++) {
-                                    if (baseIndicator[i] == 1)
-                                        sum += nu[i];
-                                }
-                                int gpIndex = groupIndex.get(new TIntArrayList(baseIndicator));
-                                C[j][k] += weights[gpIndex] * nu[j] * nu[k] / (sum*sum);
-                            }
-                        }
-                    }
-                }
-            }
-            Matrix matrix = new Matrix(C);
-            matrix = matrix.times(-1);
-            double[] eigenValues = matrix.eig().getRealEigenvalues();
-            double maxEig = -Double.MAX_VALUE;
-            int index = -1;
-            for (int i = 0; i < eigenValues.length; i++) {
-                if (eigenValues[i] > 1e-10) {
-                    System.out.println("weights = " + Arrays.toString(weights));
-                    throw new IllegalStateException();
-                }
-                if (maxEig < eigenValues[i]) {
-                    maxEig = eigenValues[i];
-                    index = i;
-                }
-            }
-            float[] eigenVector = new float[C.length];
-            for (int i = 0; i < C.length; i++) {
-                eigenVector[i] = (float) matrix.eig().getV().get(i, index);
-            }
-
-            for (int level = 1; level <= dataHandler.getNumAttributesGrouped() + 1; level++) {
-                for (int[] baseIndicator : dataHandler.getIteratorForLevel(level)) {
-                    double sum = 0;
-                    for (int i = 0; i < baseIndicator.length; i++) {
-                        if (baseIndicator[i] == 1)
-                            sum += nu[i];
-                    }
-                    int gpIndex = groupIndex.get(new TIntArrayList(baseIndicator));
-                    for (int j = 0; j < C.length; j++) {
-                        for (int k = 0; k < C.length; k++) {
-                            if (baseIndicator[j] == 1 && baseIndicator[k] == 1) {
-                                coeffs[gpIndex] -=  B * eigenVector[j] * eigenVector[k] * nu[j] * nu[k] / (sum*sum);
-                            }
-                        }
-                    }
-                }
-            }
-            return B*maxEig;
-        }
-        
-    }
-
     public double[] getEstimates() {
         return this.estimates;
     }
@@ -383,7 +208,6 @@ public class MMDConsistency {
                 }
             }
             return Math.sqrt((total * total) / sum);
-//            return weights[groupIndex.get(new TIntArrayList(baseIndicator))];
         }
         case ALLEQUAL:
         default:
